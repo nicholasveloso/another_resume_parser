ac11d5d4fdb80dc89eaef883a4ec5192acb39795
AB Kiran <18abkiran5o5@gmail.com>
2023-03-01 10:19:06 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,63 @@
+"""
+Constants for this heper module
+"""
+CONST_OUTPUT = {
+    "name": "",
+    "position": "",
+    "location": "",
+    "email": "",
+    "phone_number": "",
+    "summary": [],
+    "education": [],
+    "experiences": [],
+    "skills": [],
+    "projects": [],
+    "achievement": [],
+    "awards": [],
+    "publications": [],
+    "interests": [],
+    "soft_skills": [],
+    "volunteer_work": [],
+    "courses": [],
+    "activities": [],
+    "languages": [],
+    "linkedin": "",
+}
+EDU_DIC = {
+    "ID": 0,
+    "degree": "",
+    "startDate": "",
+    "endDate": "",
+    "school": "",
+    "cgpa": "",
+    "fieldOfStudy": "",
+    "cgpaTotal": 10.0,
+    "location": "",
+}
+EXP_DIC = {
+    "company": "",
+    "title": "",
+    "location": "",
+    "description": "",
+    "startDate": "",
+    "endDate": "",
+    "links": "",
+    "skillsUsed": [],
+}
+PROJ_DIC = {
+    "name": "",
+    "description": "",
+    "keywords": [""],
+    "startDate": "",
+    "endDate": "",
+    "url": "",
+}
+CERT_DIC = {
+    "title": "",
+    "issuer": "",
+    "skillsLearned": [],
+    "skillUsed": "",
+    "description": "",
+    "certiLink": "",
+    "courseLink": "",
+}
